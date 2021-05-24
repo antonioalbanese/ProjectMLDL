bbf21164f77586ac1569f882983d21af61acb19b
Leonardo Maggio <48280683+LeoMaggio@users.noreply.github.com>
2021-05-24 15:33:29 +0200

    train_model returns logs

--- model/trainer.py
+++ model/trainer.py
@@ -6,9 +6,10 @@ import torch.nn as nn
 from torch.backends import cudnn
 from copy import deepcopy
 from copy import copy
-import torch.optim as optim
+
 #(self, device, net, param_opt, LR, MOMENTUM, WEIGHT_DECAY, MILESTONES, GAMMA, train_dl, val_dl, test_dl)
 #(self, device, net, criterion, optimizer, scheduler, train_dl, validation_dl, test_dl):
+
 class Trainer():
   def __init__(self, device, net, LR, MOMENTUM, WEIGHT_DECAY, MILESTONES, GAMMA, train_dl, validation_dl, test_dl):
 
@@ -20,8 +21,8 @@ class Trainer():
     self.GAMMA = GAMMA
 
     self.net = net
-    self.best_net = self.net
-    
+    self.best = self.net
+
     self.criterion = nn.BCEWithLogitsLoss()
     self.parameters_to_optimize = self.net.parameters()
     self.optimizer = optim.SGD(self.parameters_to_optimize, lr=self.START_LR, momentum=self.MOMENTUM, weight_decay=self.WEIGHT_DECAY)
@@ -31,38 +32,45 @@ class Trainer():
     self.train_dl = train_dl
     self.validation_dl = validation_dl
     self.test_dl = test_dl
-
     
   def train_model(self, num_epochs):
     cudnn.benchmark
-    epoch_losses = [[float for k in range(num_epochs)] for j in range(10)]
-    test_acc_list = [float for k in range(10)]
+    
+    logs = {'group_train_loss': [float for j in range(10)],
+             'group_train_accuracies': [float for j in range(10)],
+             'predictions': [int],
+             'test_accuracies': [float for j in range(10)],
+             'true_labels': [int],
+             'val_accuracies': [float for j in range(10)],
+             'val_losses': [float for j in range(10)]}
     for g in range(10):
       self.net.to(self.DEVICE)
       #inizialize LR and STEP
       self.parameters_to_optimize = self.net.parameters()
       self.optimizer = optim.SGD(self.parameters_to_optimize, lr=self.START_LR, momentum=self.MOMENTUM, weight_decay=self.WEIGHT_DECAY)
       self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=self.MILESTONES, gamma=self.GAMMA)
-      
+
       best_acc = 0
       self.best_net = deepcopy(self.net)
 
       # ad ogni epoca chiamo il train, salvo le loss e trovo il modello migliore con l'evaluation
       for epoch in range(num_epochs):
-        e_loss = self.train_epoch(g)
-        epoch_losses[g][epoch]=e_loss
+        e_loss, e_acc = self.train_epoch(g)
         print(f"Epoch[{epoch}] loss: {e_loss} LR: {self.scheduler.get_last_lr()}")
+        
         validate_loss, validate_acc = self.validate(g)
         print(f"Validation on group[{g}] of 10 classes")
         print(f"val loss: {validate_loss}")
         print(f"val acc: {validate_acc}")
         self.scheduler.step()
+        
         if validate_acc > best_acc:
           best_acc = validate_acc
           self.best_net = deepcopy(self.net)
           best_epoch = epoch
           print("Best model updated")
         print("")
+        
 
       print(f"Group[{g}]Finished!")
       print(f"Best model at epoch {best_epoch}, best accuracy: {best_acc:.2f}")
@@ -72,21 +80,30 @@ class Trainer():
       print("")
       print("=============================================")
       print("")
-      test_acc_list[g] = test_accuracy
+      logs['group_train_loss'][g] = e_loss
+      logs['group_train_accuracies'][g] = e_acc
+      logs['val_losses'][g] = validate_loss
+      logs['val_accuracies'][g] = validate_acc
+      logs['test_accuracies'][g] = test_accuracy
 
       if g < 9:
         self.increment_classes()
 
-    return epoch_losses, test_acc_list, true_targets, predictions
+    logs['true_labels'] = true_targets
+    logs['predictions'] = predictions
+    return logs
 
   def train_epoch(self, classes_group_idx):
     self.net.train()
+
     running_loss = 0
+    running_corrects = 0
+    total = 0
     # per ogni gruppo di classi in train mi prendo le labels e le immagini
     # azzero i gradienti, mi sposto sulla gpu e faccio onehot delle labels
     # calcolo output, loss, etc.
     # alla fine di tutti i batches aggiorno la epoch_loss
-    for _,images, labels in self.train_dl[classes_group_idx]:
+    for _, images, labels in self.train_dl[classes_group_idx]:
       self.optimizer.zero_grad()
 
       images = images.to(self.DEVICE)
@@ -96,18 +113,23 @@ class Trainer():
 
       output = self.net(images)
       loss = self.criterion(output, one_hot_labels)
-      loss.backward()
-      self.optimizer.step()
 
       running_loss += loss.item()
+      _, preds = torch.max(output.data, 1)
+      running_corrects += torch.sum(preds == labels.data).data.item()
+      total += labels.size(0)
+      
+      loss.backward()
+      self.optimizer.step()
     else:
       epoch_loss = running_loss/len(self.train_dl[classes_group_idx])
+      epoch_acc = running_corrects/float(total)
       
-    return epoch_loss
+    return epoch_loss, epoch_acc
   
   def validate(self, classes_group_idx):
     self.net.train(False)
-    running_loss=0
+    running_loss = 0
     running_corrects = 0
     total = 0
     # come train(), ma prendo anche le predictions per fare qualche statistica
@@ -128,9 +150,7 @@ class Trainer():
       running_corrects += torch.sum(preds == labels.data).data.item()
     else:
       val_loss = running_loss/len(self.validation_dl[classes_group_idx])
-
-
-    val_accuracy = running_corrects / float(total)
+      val_accuracy = running_corrects / float(total)
 
 
     return val_loss, val_accuracy
