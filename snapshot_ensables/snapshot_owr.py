444cb2ed39829b8b36bc7f9c54c9adfc69e4914f
antonioalbanese <74200967+antonioalbanese@users.noreply.github.com>
2021-07-11 15:58:54 +0200

    Update snapshot_owr.py

--- snapshot_ensables/snapshot_owr.py
+++ snapshot_ensables/snapshot_owr.py
@@ -374,7 +374,7 @@ class SnapshotEnsembleOWRClassifier(_BaseSnapshotEnsemble, BaseClassifier):
 
     @torchensemble_model_doc(item="predict")
     def predict(self, *x):
-        for est in self._estimators:
+        for est in self.estimators_:
           print(est.fc.out_features)
         return super().predict(*x)
 
