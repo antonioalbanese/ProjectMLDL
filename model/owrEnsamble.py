fix softmax

model/owrEnsamble.py
@@ -157,11 +157,9 @@ class owrEnsemble(iCaRL):
       all_values = torch.cat((all_values.to(self.DEVICE),values.to(self.DEVICE)))
       all_targets = torch.cat((all_targets.to(self.DEVICE), labels.to(self.DEVICE)), dim=0)
       label_unknow_tensor = torch.tensor([unknowkn_class for _ in range(labels.size(0))]).to(self.DEVICE)
-      print(f"pred_vars: {pred_vars}")
-      print(f"values: {values}")
+
       for k,threshold in enumerate(threshold_list):
         stats = (values - threshold)/(torch.sqrt(pred_vars)/sqrt(self.n_estimators))
-        print(f"stats: {stats}")
         below_mask = stats < 2
         preds_with_unknown = torch.where(below_mask.to(self.DEVICE), torch.tensor(unknowkn_class).to(self.DEVICE), preds.to(self.DEVICE))
         running_corrects_list[k] += torch.sum(preds_with_unknown == label_unknow_tensor.data).data.item()
@@ -170,8 +168,6 @@ class owrEnsemble(iCaRL):
     for corr in running_corrects_list:
       accuracies.append(corr/float(total))
 
-    print(f"running_corrects_list: {running_corrects_list}")
-    print(f"raccuracies: {accuracies}")
 
     return accuracies, all_targets, preds_with_unknown_list, all_values
 
@@ -207,11 +203,9 @@ class owrEnsemble(iCaRL):
 
       all_values = torch.cat((all_values.to(self.DEVICE),values.to(self.DEVICE)))
       all_targets = torch.cat((all_targets.to(self.DEVICE), labels.to(self.DEVICE)), dim=0)
-      print(f"pred_vars: {pred_vars}")
-      print(f"values: {values}")
+      
       for k,threshold in enumerate(threshold_list):
         stats = (values - threshold)/(torch.sqrt(pred_vars)/sqrt(self.n_estimators))
-        print(f"stats: {stats}")
         below_mask = stats < 2
         preds_with_unknown = torch.where(below_mask.to(self.DEVICE), torch.tensor(unknowkn_class).to(self.DEVICE), preds.to(self.DEVICE))
         running_corrects_list[k] += torch.sum(preds_with_unknown == labels.data).data.item()
@@ -221,8 +215,6 @@ class owrEnsemble(iCaRL):
     for corr in running_corrects_list:
       accuracies.append(corr/float(total))
 
-    print(f"running_corrects_list: {running_corrects_list}")
-    print(f"raccuracies: {accuracies}")
 
     return accuracies, all_targets, preds_with_unknown_list, all_values
     